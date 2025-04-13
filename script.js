8be18b046796bed19c5c62ac7f13e7778a7450de
Suham <165378847+suhamofalltrades@users.noreply.github.com>
2025-04-13 17:17:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const API_KEY = "AIzaSyAtZFCfta4V-SyQXenQYI6wElUcos5kULs"; // Replace with your 
 const CX = "568b6415b7aa74ca9"; // Replace with your custom search engine ID
 // Replace with your Custom Search Engine ID
 
+const timeDiv = document.getElementById("searcTime");
+
 let currentStartIndex = 1;
 let currentQuery = "";
 
@@ -16,6 +18,7 @@ window.onload = () => {
     document.getElementById("searchBox").value = query;
     currentQuery = query;
     performSearch();
+    performImageSearch();
   }
 };
 
@@ -34,27 +37,78 @@ function performSearch() {
           div.className = "result";
           div.innerHTML = `
             <a href="${item.link}" target="_blank"><h3>${item.title}</h3></a>
-            <p>${item.snippet}</p>
+            <p>${item.snippet}</p>            
           `;
           resultsDiv.appendChild(div);
-        });
+        showButton()                          
+        }); 
+      
       } else {
         resultsDiv.innerHTML = "<p>No results found.</p>";
       }
     })
     .catch(err => {
       console.error("Search failed", err);
-    });
-}
+    
+    });}
 
+function performImageSearch() {
+  if (!currentQuery) return;
+    
+    fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CX}&q=${encodeURIComponent(currentQuery)}&searchType=image&start=${currentStartIndex}`)
+      .then(res => res.json())
+      .then(data => {
+        const imagesDiv = document.getElementById("images");
+        imagesDiv.innerHTML = "";
+    
+        if (data.items) {
+          data.items.forEach(item => {
+            const div = document.createElement("div");
+            div.className = "image";
+            div.innerHTML = `
+              <a href="${item.image.contextLink}" target="_blank"><img src="${item.link}" alt="${item.title}"></a>            
+            `;
+          imagesDiv.appendChild(div);
+        showButton() 
+        tab()                         
+        }); 
+          
+      } else {
+        resultsDiv.innerHTML = "<p>No results found.</p>";
+      }
+    })
+    .catch(err => {
+      console.error("image Search failed", err);
+        
+    });}
+function showButton() {  
+  document.getElementById("page").style.visibility = "visible"
+  document.getElementById("about").style.display = "none"
+}
 function nextPage() {
   currentStartIndex += 10;
   performSearch();
+  performImageSearch();
 }
 
 function prevPage() {
   if (currentStartIndex > 1) {
     currentStartIndex -= 10;
     performSearch();
+    performImageSearch();
   }
+}
+
+function webTab() {
+  document.getElementById("results").style.display = "block"
+  document.getElementById("images").style.display = "none"
+}
+
+function imageTab() {
+  document.getElementById("results").style.display = "none"
+  document.getElementById("images").style.display = "flex"
+}
+
+function tab() {
+  document.getElementById("tab").style.visibility = "visible"
 }
